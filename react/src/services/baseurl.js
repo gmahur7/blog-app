b9de78ee95dd8f68f46ecd1473c40e61db48e610
gmahur7 <gm7017308@gmail.com>
2024-12-09 14:06:29 +0530

    first

--- react/src/services/baseurl.js
+++ react/src/services/baseurl.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5001"
+const API_URL = "https://blog-app-backend-apis.vercel.app"
+// const API_URL = "http://localhost:5001"
 
 const api = axios.create({
     baseURL: API_URL,
